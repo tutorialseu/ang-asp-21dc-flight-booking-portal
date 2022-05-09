6ecfb1f55300dd481770176eb3199cbe91e0594d
JannickL <jannick.leismann@gmx.de>
2022-05-09 13:48:01 +0200

    4. Day Commit
    Added interfaces and search-flight styling

--- Flights/ClientApp/src/app/search-flights/search-flights.component.ts
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -1,3 +1,4 @@
+import { Time } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFlightsComponent implements OnInit {
 
+
+  searchResult: FlightRm[] = [
+    {
+      airline: "American Airlines",
+      remainingNumberOfSeats: 500,
+      departure: { time: Date.now().toString(), place: "Los Angeles" },
+      arrival: { time: Date.now().toString(), place: "Istanbul" },
+      price: "350",
+    },
+    {
+      airline: "Deutsche BA",
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: "Munchen" },
+      arrival: { time: Date.now().toString(), place: "Schiphol" },
+      price: "600",
+    },
+    {
+      airline: "British Airways",
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: "London, England" },
+      arrival: { time: Date.now().toString(), place: "Vizzola-Ticino" },
+      price: "600",
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+export interface FlightRm {
+  airline: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNumberOfSeats: number;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
